8aadf7e3ca678f7b0626fd370f6d909f4d73d60a
yorgos <tsakirakii@gmail.com>
2024-07-08 03:53:29 +0300

    Added Index (Events)

--- Soft_Eng_Spring2024/Models/Event.cs
+++ Soft_Eng_Spring2024/Models/Event.cs
@@ -1,9 +1,12 @@
-﻿namespace Soft_Eng_Spring2024.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Soft_Eng_Spring2024.Models
 {
     public class Event
     {
         public int Id { get; set; }
-        public DateOnly Date { get; set; }= DateOnly.FromDateTime(DateTime.Now);
+        [DataType(DataType.Date)]
+        public DateOnly Date { get; set; }
         public string Title { get; set; }
         public string Body { get; set; }
         public int Author { get; set; }
